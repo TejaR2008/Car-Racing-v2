58333878fbcf6ba12ad92397a16c89b7caf97fce
TejaR2008 <74098883+TejaR2008@users.noreply.github.com>
2021-02-12 18:30:39 +0530

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,50 @@
+class Game {
+    constructor(){
+
+    }
+
+    getState(){
+        var gameStateref = database.ref('gameState')
+        gameStateref.on("value", function(data){
+            gameState = data.val()
+        })
+    }
+
+    update(state){
+        database.ref('/').update({
+            gameState: state
+        })
+    }
+
+    start(){
+        if(gameState === 0){
+            player = new Player()
+            player.getCount()
+            form = new Form()
+            form.display();
+        }
+    }
+    play(){
+        form.hide()
+        textSize(30);
+        text("Game Start", 120, 100);
+        Player.getPlayerInfo();
+        if(allPlayers != undefined){
+            var displayPosition = 130; 
+            for(var pl in allPlayers){
+                if(pl === "player" + player.index)
+                fill("red");
+                else 
+                fill("black")
+                displayPosition += 20
+                textSize(15);
+                text(allPlayers[pl].name + ":" + allPlayers[pl].distance, 120, displayPosition)
+            }
+        }
+        if(keyIsDown(UP_ARROW) && player.index != null){
+            player.distance += 50
+            player.update();
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Car-Racing-v2
+# asynchronousBallMovement
+Boiler plate for ball moving aysnchronously
